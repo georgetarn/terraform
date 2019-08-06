17ef8226ba0b1836b19894553fb95550c49bb2ca
David Linares <David.Linares@automationlogic.com>
2019-08-06 13:51:52 +0100

    Move variables in a separate file

---
+++ basic/variables.tf
@@ -0,0 +1,29 @@
+variable "profile" {
+  default     = "xxx"
+  description = "AWS local profile credentials"
+}
+
+variable "prefix" {
+  default     = "yourname"
+  description = "Cluster Prefix - All resources created by Terraform have this prefix prepended to them"
+}
+
+variable "count_ec2_instance_nodes" {
+  default     = "0"
+  description = "Number of ec2 nodes"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS Region for deployment"
+}
+
+variable "ec2_instance_node_type" {
+  default     = "t2.large"
+  description = "AWS instance type for the ec2 nodes"
+}
+
+variable "ssh_key_name" {
+  default     = ""
+  description = "AWS Key Pair Name"
+}
