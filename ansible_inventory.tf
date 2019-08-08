5367c4990a89318863bb4dc4ccf6191ca57f7b5a
David Linares <David.Linares@automationlogic.com>
2019-08-08 10:53:35 +0100

    Launch ansible provisioning from terraform

--- ansible_inventory.tf
+++ ansible_inventory.tf
@@ -9,3 +9,38 @@ resource "local_file" "inventory-generated" {
   content  = "${data.template_file.inventory.rendered}"
   filename = "./ansible/inventory-generated"
 }
+
+
+resource "null_resource" "ensure_connectivity" {
+  count           = "${var.count_ec2_instance_nodes}"
+
+  triggers = {
+    nodes_created  = "${join(",", aws_instance.ec2_instance.*.id)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host = "${aws_instance.ec2_instance.*.public_ip[count.index]}"
+    private_key = "${tls_private_key.tls_key.private_key_pem}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y"
+    ]
+  }
+}
+
+# Trigger ansible provisioning when all the nodes have connectivity
+resource "null_resource" "ansible" {
+  triggers = {
+    nodes_created  = "${join(",", null_resource.ensure_connectivity.*.id)}"
+    inventory_created = "${local_file.inventory-generated.id}"
+  }
+
+  provisioner "local-exec" {
+      working_dir = "ansible"
+      command = "ansible-playbook -i inventory-generated sensu-cluster-playbook.yml -v"
+  }
+}
